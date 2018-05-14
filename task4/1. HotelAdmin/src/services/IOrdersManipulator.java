04277c26a5b2f56b5ca26eeda4e74f309564d01a
konstantin-np <decoy4157@yandex.ru>
2018-05-14 11:51:43 +0300

    Added methods to IOrdersManipulator interface

--- task4/1. HotelAdmin/src/services/IOrdersManipulator.java
+++ task4/1. HotelAdmin/src/services/IOrdersManipulator.java
@@ -2,10 +2,17 @@ package services;
 
 import java.util.List;
 
+import models.Client;
+import models.Order;
+import models.Service;
 import stores.OrdersStore;
 
 public interface IOrdersManipulator {
 	public List<String> getClientsAndRooms(OrdersStore orders);
-	
-	
+	public long calcStayingDays(Order o);
+	public double calcStayingPrice(Order o);
+	public double calcServicesPrice(Order o);
+	public double calcSumPrice(Order o);
+	public List<Order> getPreviousRoomers(OrdersStore orders, int roomId);
+	public List<Service> getClientServices(OrdersStore orders, Client client);
 }
